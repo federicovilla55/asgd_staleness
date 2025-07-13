b034db4937c38ab3641e116d540939c6766a4709
federicovilla55 <federicovilla55@users.noreply.github.com>
2025-07-13 20:18:06 +0000

    deploy: 910cef7398eba4ded3b55b7c0cb32effd6297086

--- src/core/data_types.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-class ParameterServerStatus(Enum):
-    """
-    Enum for the status of the parameter server.
-    """
-    ACCEPTED = 0
-    REJECTED = 1
-    SHUTDOWN = 2
--- src/data/__init__.py
+++
@@ -1,9 +0,0 @@
-from .linear import LinearRegressionBuilder
-from .poly import PolyVariedBuilder
-from .full import FullDataLoaderBuilder
-from .datasets import (
-    load_linear_data,
-    load_poly_varied_data,
-    create_linear_data_loader,
-    create_poly_varied_data_loader,
-)
